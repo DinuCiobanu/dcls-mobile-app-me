9b333c72cfe711b63694861f851650407c547e7c
Bayleaf <152853920+bayleaf1@users.noreply.github.com>
2025-06-19 21:59:13 +0300

    cleanup comments

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,8 +4,6 @@ const Modal = (): React.ReactElement => {
   return (
     <ModalConsumer>
       {({ component: Component, hideModal, props }) => {
-        // console.log('53-36', Component.toString())
-        // return <View style={{ width: 100, height: 200, backgroundColor: 'red' }}></View>
         return Component ? <Component {...props} onRequestClose={hideModal} /> : null
       }}
     </ModalConsumer>
